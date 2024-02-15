dee926faaaca8f6b65c5a70a84d2427f8e22bfc8
Taras Tarasiuk <taras_tarasiuk@epam.com>
2024-02-15 20:58:47 +0200

    [#16] Pagination & Filtering via url params

--- src/App.vue
+++ src/App.vue
@@ -57,10 +57,29 @@
     </section>
 
    <template v-if="tickers.length > 0" >
+     <hr class="w-full border-t border-gray-600 my-4" />
+     <div>
+        <button
+            v-if="page > 1"
+            @click="page = page - 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Back
+        </button>
+        <button
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Next
+        </button>
+        <div>
+          Filter:
+          <input v-model="filter"/>
+        </div>
+      </div>
       <hr class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
-          v-for="t in tickers" 
+          v-for="t in filteredTickers()"
           :key="t.name"
           @click = "select(t)"
           :class="{
@@ -97,7 +116,7 @@
         </div>
       </dl>
       <hr class="w-full border-t border-gray-600 my-4" />
-    </template> 
+    </template>
     <section v-if="sel" class="relative">
       <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
         {{ sel.name }} - USD
@@ -150,11 +169,24 @@ export default {
       ticker: "",
       tickers: [],
       sel : null,
-      graph: []
+      graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
     };
   },
 
   created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem('cryptonomicon-list')
 
     if (tickersData) {
@@ -166,6 +198,20 @@ export default {
   },
 
   methods: {
+
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(
+          ticker => ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredTickers.length > end
+
+      return filteredTickers.slice(start, end);
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -191,6 +237,7 @@ export default {
       };
 
       this.tickers.push(currentTicker)
+      this.filter = ''
 
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
       this.subscribeToUpdates(currentTicker.name)
@@ -199,7 +246,7 @@ export default {
     },
 
     handleDelete(ticketToRemove) {
-      this.tickers = this.tickers.filter(t => t != ticketToRemove)
+      this.tickers = this.tickers.filter(t => t !== ticketToRemove)
     },
 
     normalizedGraph() {
@@ -214,6 +261,24 @@ export default {
       this.sel = selectedTicker,
       this.graph = [];
     },
+  },
+
+  watch: {
+    filter() {
+      this.page = 1
+      window.history.pushState(
+          null,
+          document.title,
+          `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
+
+    page() {
+      window.history.pushState(
+          null,
+          document.title,
+          `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
+
   }
 
 }
