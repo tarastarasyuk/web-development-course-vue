5d16edb819d4f35fcc8b1f51ecf85e426c7e369e
Taras Tarasiuk <taras_tarasiuk@epam.com>
2024-02-24 20:56:43 +0200

    [#18] Moving from http to websockets: an example how with extracted API logic the business vue component is not changed in case of some API changes - changes only in api.js

--- src/api.js
+++ src/api.js
@@ -2,46 +2,65 @@ const API_KEY =
     '4caa95b1312b4ddfca598bf879d242dd3c2c8cc635c161e4e515758bd82717e9'
 
 const tickersHandlers = new Map(); // { }
+const socket = new WebSocket(
+    `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+)
 
-// TODO: refactor to use UrlSearchParams - current version is not safe
-const loadTickers = () => {
-    if (tickersHandlers.size === 0) {
+const AGGREGATE_INDEX = "5";
+
+socket.addEventListener('message', e => {
+    const {TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(
+        e.data
+    );
+    if (type !== AGGREGATE_INDEX) {
         return;
     }
 
-    fetch(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[
-            ...tickersHandlers.keys()]
-            .join(
-                ","
-            )}&tsyms=USD&api_key=${API_KEY}`
-    ).then(r => r.json())
-        // this logic should be in this API call part so that, so that component
-        // just call the function without diving into tech details
-        // *generally some mapping api data mappings or modifications should be performed in specific
-        // component based on the business reqs, but in this case it just technical logic
-        // to normalize API response data
-        .then(rawData => {
-            const updatedPrices = Object.fromEntries(
-                Object.entries(rawData).map(([key, value]) => [key, value.USD])
-            )
-            Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-                const handlers = tickersHandlers.get(currency) ?? [];
-                handlers.forEach(fn => fn(newPrice))
-            })
-        })
+    const handlers = tickersHandlers.get(currency) ?? [];
+    handlers.forEach(fn => fn(newPrice))
+})
+
+function sendToWebSocket(message) {
+    const stringifiedMessage = JSON.stringify(message);
+
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(stringifiedMessage);
+        return;
+    }
+
+    socket.addEventListener(
+        'open',
+        () => {
+            socket.send(stringifiedMessage)
+        },
+        {once: true}
+    );
+}
+
+function subscribeToTickerOnWs(ticker) {
+    sendToWebSocket({
+        "action": "SubAdd",
+        "subs": [`5~CCCAGG~${ticker}~USD`]
+    });
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+    sendToWebSocket({
+        "action": "SubRemove",
+        "subs": [`5~CCCAGG~${ticker}~USD`]
+    });
 }
 
 export const subscribeToTicker = (ticker, cb) => {
     const subscribers = tickersHandlers.get(ticker) || [];
     tickersHandlers.set(ticker, [...subscribers, cb]);
+    subscribeToTickerOnWs(ticker);
 }
 
 export const unsubscribeFromTicker = ticker => {
     tickersHandlers.delete(ticker);
+    unsubscribeFromTickerOnWs(ticker);
 }
-
-setInterval(loadTickers, 5000)
 
 window.tickers = tickersHandlers
 // the idea of API call extraction is not in receiving the crypto prices
