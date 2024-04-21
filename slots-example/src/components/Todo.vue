48ce9d67389bd28461147ee244bf3ebd8780d428
Taras Tarasiuk <taras_tarasiuk@epam.com>
2024-04-21 11:20:37 +0300

    [#27] Slots example

---
+++ slots-example/src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="checkbox" :checked="item.completed" />
+  {{ item.title }}
+</template>
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
