25f8679cc50e111a03cbf5265c7fa431a147ef4a
Taras Tarasiuk <taras_tarasiuk@epam.com>
2024-04-21 12:21:53 +0300

    [#27][hw] - slots practise project init

---
+++ slots-practise/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="isPopupOpen = true">Open window</button>
+  <PopupWindow
+      :is-open="isPopupOpen"
+      @ok="popupConfirmed"
+      @close="isPopupOpen = false">
+    Are you sure?
+  </PopupWindow>
+</template>
+
+<script>
+import PopupWindow from "@/components/PopupWindow.vue";
+
+export default {
+  components: {PopupWindow},
+
+  data() {
+    return {
+      isPopupOpen: false
+    }
+  },
+
+  methods: {
+    popupConfirmed() {
+      alert("Confirmed!")
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
