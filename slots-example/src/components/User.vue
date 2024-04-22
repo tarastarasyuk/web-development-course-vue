d3ab6f061ca448094f96fd662cfaf8d22e3b3fef
Taras Tarasiuk <worktarasyuk@gmail.com>
2024-04-22 20:03:29 +0300

    [#27] Slots example

---
+++ slots-example/src/components/User.vue
@@ -0,0 +1,11 @@
+<template>Specific User component: {{ item.name }} ({{ item.username }})</template>
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
