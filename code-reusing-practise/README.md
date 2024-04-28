61ec0ad92715eac0b1d7316248f7129e63ccf1d7
Taras Tarasiuk <taras_tarasiuk@epam.com>
2024-04-28 13:09:51 +0300

    [#33] Code reusing: adding a new API integration - an example of similar code *almost the same setup as for techCrunch *proxy used to avoid issues with swapi.dev ssl

--- code-reusing-practise/README.md
+++ code-reusing-practise/README.md
@@ -3,6 +3,8 @@
 ## Project setup
 ```
 npm install
+
+node proxy/server.js
 ```
 
 ### Compiles and hot-reloads for development
--- code-reusing-practise/src/components/StarWars.vue
+++ code-reusing-practise/src/components/StarWars.vue
@@ -1,3 +1,89 @@
 <template>
-  <div>stub</div>
+  <div>
+    <label>Search: <input v-model.lazy="search" type="text"></label>
+    <hr/>
+    <div>
+      <button :disabled="page === 1" @click="page -= 1">Prev</button>
+      {{ page }}
+      <button @click="page += 1">Next</button>
+    </div>
+    <hr/>
+
+    <ul>
+      <!-- eslint-disable-next-line -->
+      <li v-for="item in items">
+        <a target="_blank" :href="item.link">{{ item.name }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
+<script>
+import {getPlanets} from "@/api/swapi";
+
+export default {
+  data() {
+    return {
+      items: [],
+      search: '',
+      page: 1,
+    };
+  },
+
+  methods: {
+
+    async loadPlanets() {
+      // using { results } is not safe, we do not handle requests exception to be focused on global problem
+      const { results } = await getPlanets({
+        page: this.page,
+        search: this.search
+      });
+      this.items = results;
+    },
+
+    syncHash() {
+      const urlParams = new URLSearchParams(window.location.hash.substring(1));
+      const entries = Object.fromEntries(urlParams.entries());
+      if (entries.page) {
+        this.page = entries.page;
+      }
+      if (entries.search) {
+        this.search = entries.search;
+      }
+    },
+
+    updateHash() {
+      const urlParams = new URLSearchParams();
+      if (this.page !== 1) {
+        urlParams.append("page", this.page);
+      }
+      if (this.search !== "") {
+        urlParams.append("search", this.search);
+      }
+
+      window.location.hash = urlParams.toString();
+    },
+  },
+
+  created() {
+    window.addEventListener("hashchange", this.syncHash);
+    this.syncHash();
+    this.loadPlanets();
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("hashchange", this.syncHash);
+  },
+
+  watch: {
+    page() {
+      this.loadPlanets();
+      this.updateHash();
+    },
+    search() {
+      this.page = 1;
+      this.loadPlanets();
+      this.updateHash();
+    },
+  },
+};
+</script>
